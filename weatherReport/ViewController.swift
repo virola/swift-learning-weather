edf2a95c4e62bcea409f14cbe783507450aa9463
Virola <virola.zhu@gmail.com>
2016-06-17 16:24:31 +0800

    Initial Commit

---
+++ weatherReport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherReport
+//
+//  Created by Virola on 16/6/17.
+//  Copyright © 2016年 Virola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
